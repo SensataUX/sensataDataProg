a9c55742d720fb20ff16a0b08457c03c6f6e31cc
Gabriel N. Camargo-Toledo <gcamargo@sensata.io>
2021-10-13 16:58:40 -0500

    update manual

--- R/sensataDataProg-package.R
+++ R/sensataDataProg-package.R
@@ -1,6 +1,8 @@
-#' sensataDataProg: This package has the functions needed to clean and manipulate data created with the Sensata platform.
+#' sensataDataProg: from Mongo to R
 #'
 #' @description
+#' This package has the functions needed to clean and manipulate data created with the Sensata platform.
+#'
 #' Este paquete tiene las funciones necesarias para limpiar y manipular los datos de la plataforma Sensata.
 #'
 #' @section sensataDataProg basic functions:
@@ -15,6 +17,8 @@
 #' Además incluye funciones para otros procesamientos de datos que solemos hacer:
 #' - scrubPeaks: eliminar picos atípicos
 #' - translateFactors: traducir datos de un idioma a otro
+#'
+#' @source \url{https://github.com/GaborioSensata/sensataDataProg}
 #'
 #'
 #' @docType package
