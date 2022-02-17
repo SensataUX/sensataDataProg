7ad443b067c47695d375f1950561455dab16749c
Gabriel N. Camargo-Toledo <gcamargo@sensata.io>
2022-02-17 15:36:24 -0500

    including example data empty columns

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A dataset of survey 590 surveys responses downloaded from mongo, HGS survey
 #' sensataExample
 #'
-#' @format A data frame with 590 rows and 595 variables:
+#' @format A data frame with 590 rows and 609 variables:
 #' \describe{
 #'   \item{newresponses}{Information on responses}
 #'   \item{othercols}{other metadata}
