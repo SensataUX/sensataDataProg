ddbec52afce717a6ebadc365d5c167906739471b
Gabriel Nicolás Camargo Toledo <gaborio@pop-os.localdomain>
2023-04-03 11:32:45 -0500

    fix last lev

--- R/makeFactors.R
+++ R/makeFactors.R
@@ -202,7 +202,7 @@ makeFactors <- function(
     }
     # If special value provided -----
   if(!(isMultiple) && isClose && !is.null(specialSkipValue)){
-    df[[v == last(lev)]] <- specialSkipValue
+    df[[v]][df[[v]] == last(lev)] <- specialSkipValue
     lev <- lev[-length(lev)]
     lev <- c(1:length(lev), as.numeric(specialSkipValue))
     names(lev) <- lab
